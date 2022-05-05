ac7fffc154c4b99d0ce51b404f1c1333e552c460
djlee118 <djlee118@nate.com>
2022-05-05 11:25:40 +0900

    Initial commit  - GSON 라이브러리를 활용 프로그램

---
+++ src/main/java/com/example/gsonprogramexample/GsonProgramExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsonprogramexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsonProgramExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsonProgramExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gsonprogramexample/GsonProgramExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsonprogramexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsonProgramExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
